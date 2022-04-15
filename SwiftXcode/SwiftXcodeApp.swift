9a8bc9865f27e8cc1bfa8d188aa97e1e989e9036
James Sherlock <15193942+Sherlouk@users.noreply.github.com>
2022-04-15 13:53:55 +0100

    initial commit

---
+++ SwiftXcode/SwiftXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftXcodeApp.swift
+//  SwiftXcode
+//
+//  Created by James Sherlock on 15/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
